e442dfcb98df75ef92b45a2a985c73238e02bf73
eruva <dacic.almin2000@gmail.com>
2026-03-14 21:13:00 +0100

    lol

--- Module03/5test.py
+++
@@ -1,6 +0,0 @@
-list = ["sacha", "mateo", "maxime"]
-
-iterator = iter(list)
-
-for x in iterator:
-    print(x)
---
+++ Module03/6.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    scores = {"alice": 2300, "bob": 1800, "charlie": 2150, "diana": 2050}
+    
